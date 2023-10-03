22d0a9afb5fbb1cbf452927bd9fa8e28e5364646
KrieqqeR <burakcevizli@gmail.com>
2023-10-03 22:23:16 +0300

    ContacPageWeHelp done.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import OnlyBandage from '../layouts/OnlyBandage'
 import ContactPageGetinTouch from "../components/ContactPageGetinTouch"
+import ContactPageWeHelp from "../components/ContactPageWeHelp"
 
 export default function Contact() {
     return (
         <div>
             <OnlyBandage />
             <ContactPageGetinTouch />
+            <ContactPageWeHelp/>
         </div>
     )
 }
