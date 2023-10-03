65c1a6c2353fb86adab34b69308af0095f0c6b4e
KrieqqeR <burakcevizli@gmail.com>
2023-10-03 20:55:56 +0300

    TEAM Heroes is done.

---
+++ src/components/TeamPageHeroes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import redGirl from "../assets/TeamPageRedPicture.png"
+import WhiteGirl from "../assets/TeamPageFirstRight.png"
+import BlackGirl from "../assets/TeamPageBlack.png"
+import Picture3 from "../assets/TeamPage picture.png"
+import Picture4 from "../assets/TeamPageBlueJEan.png"
+
+export default function TeamPageHeroes() {
+    return (
+        <div className='my-20 flex'>
+            {/* Sol Kısım */}
+            <img className='min-w-[50%] mr-4' src={redGirl} alt='resim'  />
+            {/* Sağ Kısım */}
+            <div className='flex gap-4 min-w-[50%]'>
+                <div className='flex flex-col justify-center items-center h-[100%] w-[50%]'>
+                    <img src={WhiteGirl} alt='resim' className='h-[100%] w-[100%]' />
+                    <img src={BlackGirl} alt='resim' className='h-[100%] w-[100%] pt-4' />
+                </div>
+                <div className='flex flex-col justify-center items-center h-[100%] w-[50%]'>
+                    <img src={Picture3} alt='resim' className='h-[100%] w-[100%]' />
+                    <img src={Picture4} alt='resim' className='h-[100%] w-[100%] pt-4' />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import OnlyBandage from "../layouts/OnlyBandage"
 import TeamPageInnovation from '../components/TeamPageInnovation'
+import TeamPageHeroes from '../components/TeamPageHeroes'
 
 export default function Team() {
     return (
         <div>
             <OnlyBandage />
             <TeamPageInnovation />
+            <TeamPageHeroes />
         </div>
     )
 }
