0c766df1be7fbe89f1eaa97c5b8c619b3d9c76b8
KrieqqeR <burakcevizli@gmail.com>
2023-10-08 16:07:13 +0300

    FeaturedProducts is updated.

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -5,15 +5,15 @@ import Ok from "../assets/uil_arrow-growth.png"
 
 export default function FeaturedProducts() {
   return (
-    <div className='w-5/6 mx-auto mb-40'>
+    <div className='w-5/6  mx-auto mb-40'>
       {/* Üst Div */}
-      <div className='text-center mr-12'>
+      <div className='text-center mobile:w-full mobile:mx-auto mr-12'>
         <h1 className='text-[#737373] text-[1.25rem] font-semibold'>Featured Products</h1>
         <h1 className='text-[#252B42] font-bold text-[1.5rem] tracking-[0.0625rem] my-4'>THE BEST SERVICES</h1>
         <p className='text-[#737373] text-[0.875rem] font-semibold'>Problems trying to resolve the conflict between </p>
       </div>
       {/* Alt Div */}
-      <div className='flex justify-around mt-40'>
+      <div className='flex flex-wrap justify-around mobile:mt-10 mt-40'>
         <div className='text-center'>
           <img src={Kitap1} alt='kitap' className='mx-auto'/>
           <h1 className='my-8 text-[#252B42] text-[1.5rem] font-bold'>Easy Wins</h1>
