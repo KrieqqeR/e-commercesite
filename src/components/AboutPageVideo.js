c4618af07519b9edc6e373dc46f648282f540382
KrieqqeR <burakcevizli@gmail.com>
2023-10-09 21:12:22 +0300

    Auto play is fixed.

--- src/components/AboutPageVideo.js
+++ src/components/AboutPageVideo.js
@@ -7,15 +7,15 @@ export default function AboutPageVideo() {
         <div className="my-40 relative">
             <video
                 className="w-[62rem] h-[34rem] max-w-full flex mx-auto justify-center border border-gray-200 rounded-xl"
-                autoPlay
-                muted
+                autoPlay={true} // Otomatik oynatma açık
+                muted={false}   // Ses açık
                 controls
             >
                 <source src={video} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className='absolute top-52 left-[56rem] mobile:hidden'>
-                <button className="overflow-hidden bg-[#23a6f0] flex flex-col justify-center w-24 h-24 items-center rounded-[73.5999984741211px]">
+                <button className="overflow-hidden bg-[#23a6f0] flex flex-col justify-center w-24 h-24 items-center mobile:hidden rounded-[73.5999984741211px] hidden">
                     <img
                         src="https://file.rendit.io/n/V23vJlUqPotoLaHf9rdc.svg"
                         id="IcnPlayIcnsm"
