5b4b899c168e73ad8fd29c213dc18632b3a0cde7
KrieqqeR <burakcevizli@gmail.com>
2023-10-15 17:14:47 +0300

    Store customer kısmı kaldı.

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -128,16 +128,15 @@ export default function FormPage() {
 
 
                 <div className="flex flex-wrap -mx-3 mb-2">
-
                     <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0 mx-auto">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
-                            State
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="role">
+                            Role
                         </label>
                         <div className="relative">
-                            <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                <option>New Mexico</option>
-                                <option>Missouri</option>
-                                <option>Texas</option>
+                            <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="role">
+                                <option>Customer</option>
+                                <option>Store</option>
+                                
                             </select>
                             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -145,7 +144,7 @@ export default function FormPage() {
                         </div>
                     </div>
                 </div>
-                <button>Sıgn up</button>
+                <button className=" mt-12 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 ">Sıgn up</button>
             </form>
         </div>
     );
