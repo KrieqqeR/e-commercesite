6af80d46ae937f480392e83e0705661f1babac31
KrieqqeR <burakcevizli@gmail.com>
2023-10-16 13:13:35 +0300

    Onchange eklendi.

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function FormPage() {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({mode:"onChange"});
     let [loading, setLoading] = useState(false);
     let [color, setColor] = useState("#ffffff");
     const history = useHistory();
