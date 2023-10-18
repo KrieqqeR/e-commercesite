8edf25734545f48187779c65cb194fb2f9c75bbf
KrieqqeR <burakcevizli@gmail.com>
2023-10-18 22:07:54 +0300

    Admin featured added...

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -29,7 +29,7 @@ export default function FormPage() {
 
     const onSubmit = (data) => {
         const formData = {};
-        if (selectedRole === "Customer") {
+        if (selectedRole === "Customer" || selectedRole === "Admin") {
             formData.name = data.name;
             formData.email = data.email;
             formData.password = data.password;
@@ -93,14 +93,14 @@ export default function FormPage() {
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full md:w-1/2 px-3 mb-6 mx-auto md:mb-0">
                             <label
-                                className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                                className="block uppercase tracking-wide text-blue-900 text-xs font-bold mb-2"
                                 htmlFor="name"
                             >
                                 Name
                             </label>
                             <input
-                                className={`appearance-none block w-full bg-gray-200 text-white border ${errors.email ? "border-red-500" : "border-gray-200"
-                                    } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+                                className={`appearance-none block w-full bg-gray-200 text-blue-900 border ${errors.email ? "border-red-500" : "border-gray-200"
+                                    } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 font-bold`}
                                 id="name"
                                 type="text"
                                 placeholder="Name"
@@ -123,10 +123,10 @@ export default function FormPage() {
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2 " htmlFor="email">
+                            <label className="block uppercase tracking-wide text-blue-900 text-xs font-bold mb-2 " htmlFor="email">
                                 E-mail
                             </label>
-                            <input className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${errors.email ? "border-red-500" : "border-gray-200"
+                            <input className={`appearance-none block w-full bg-gray-200 text-blue-900 border ${errors.email ? "border-red-500" : "border-gray-200"
                                 } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`} id="grid-password" type="email" placeholder="abc@gmail.com" {...register("email", { required: "Email should be written", pattern: { value: /^\S+@\S+$/i, message: "Enter an invalid email address" } })}
                             />
 
@@ -142,11 +142,11 @@ export default function FormPage() {
 
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-white  text-xs font-bold mb-2" htmlFor="password">
+                            <label className="block uppercase tracking-wide text-blue-900  text-xs font-bold mb-2" htmlFor="password">
                                 Password
                             </label>
                             <input
-                                className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${errors.password ? "border-red-500" : "border-gray-200"} rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+                                className={`appearance-none block w-full bg-gray-200 text-blue-900 border ${errors.password ? "border-red-500" : "border-gray-200"} rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
                                 placeholder='*************'
                                 id="password"
                                 type="password"
@@ -184,10 +184,10 @@ export default function FormPage() {
 
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-white  text-xs font-bold mb-2" htmlFor="passwordMatch">
+                            <label className="block uppercase tracking-wide text-blue-900  text-xs font-bold mb-2" htmlFor="passwordMatch">
                                 Confirm Password
                             </label>
-                            <input className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${errors.passwordMatch ? "border-red-500" : "border-gray-200"
+                            <input className={`appearance-none block w-full bg-gray-200 text-blue-900 border ${errors.passwordMatch ? "border-red-500" : "border-gray-200 "
                                 } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`} id="passwordMatch" type="password" placeholder="Confirm Password" {...register("passwordMatch", {
                                     required: "Confirm Password is required",
                                     validate: (value) => value === password || "Passwords must match",
@@ -209,16 +209,17 @@ export default function FormPage() {
 
                     <div className="flex flex-wrap -mx-3 mb-2">
                         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0 mx-auto">
-                            <label className="block uppercase tracking-wide text-white  text-xs font-bold mb-2" htmlFor="role">
+                            <label className="block uppercase tracking-wide text-blue-900  text-xs font-bold mb-2" htmlFor="role">
                                 Role
                             </label>
                             <div className="relative">
-                                <select onChange={(e) => setSelectedRole(e.target.value)} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="role">
+                                <select onChange={(e) => setSelectedRole(e.target.value)} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-blue-900 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="role">
                                     <option>Customer</option>
+                                    <option>Admin</option>
                                     <option >Store</option>
 
                                 </select>
-                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-blue-900">
                                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                                 </div>
                             </div>
@@ -229,13 +230,13 @@ export default function FormPage() {
                             <div className="flex flex-wrap -mx-3 mb-6">
                                 <div className="w-full px-3">
                                     <label
-                                        className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                                        className="block uppercase tracking-wide text-blue-900 text-xs font-bold mb-2"
                                         htmlFor="storeName"
                                     >
                                         Store Name
                                     </label>
                                     <input
-                                        className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${errors.email ? "border-red-500" : "border-gray-200"
+                                        className={`appearance-none block w-full bg-gray-200 text-blue-900 border ${errors.email ? "border-red-500" : "border-gray-200"
                                             } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
                                         id="storeName"
                                         type="text"
@@ -259,13 +260,13 @@ export default function FormPage() {
                             <div className="flex flex-wrap -mx-3 mb-6">
                                 <div className="w-full px-3">
                                     <label
-                                        className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                                        className="block uppercase tracking-wide text-blue-900 text-xs font-bold mb-2"
                                         htmlFor="storeTaxId"
                                     >
                                         Store Tax ID
                                     </label>
                                     <input
-                                        className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${errors.email ? "border-red-500" : "border-gray-200"
+                                        className={`appearance-none block w-full bg-gray-200 text-blue-900 border ${errors.email ? "border-red-500" : "border-gray-200"
                                             } rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
                                         id="storeTaxId"
                                         type="text"
@@ -289,11 +290,11 @@ export default function FormPage() {
 
                             <div className="flex flex-wrap -mx-3 mb-6">
                                 <div className="w-full px-3">
-                                    <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="storeBankAccount">
+                                    <label className="block uppercase tracking-wide text-blue-900 text-xs font-bold mb-2" htmlFor="storeBankAccount">
                                         Store Bank Account (IBAN)
                                     </label>
                                     <input
-                                        className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${errors.storeBankAccount ? "border-red-500" : "border-gray-200"} rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+                                        className={`appearance-none block w-full bg-gray-200 text-blue-900 border ${errors.storeBankAccount ? "border-red-500" : "border-gray-200"} rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
                                         id="storeBankAccount"
                                         type="text"
                                         placeholder="TRXXXXXXXXXXXXXXXXXXXXXXXXXX"
@@ -332,7 +333,7 @@ export default function FormPage() {
                                 />
                             </div>
                         ) : null}
-                        <button type='submit' className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 leading-tight focus:outline-none focus:bg-blue-600 hover:bg-blue-500 focus:border-gray-500 ">
+                        <button type='submit' className="appearance-none block w-full bg-gray-200 text-blue-900 border rounded py-3 leading-tight focus:outline-none focus:bg-blue-600 hover:bg-blue-500 focus:border-gray-500 ">
                             {loading ? "Loading..." : "Submit"}
                         </button>
                     </div>
