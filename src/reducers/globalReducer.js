55725ad386a659d4102a59f968f05d7415798db7
KrieqqeR <burakcevizli@gmail.com>
2023-10-21 14:04:02 +0300

    global actions and global reducers are done ...

--- src/reducers/globalReducer.js
+++ src/reducers/globalReducer.js
@@ -1,1 +1,40 @@
-const initialValue  ={}
+import {
+    SET_CATEGORY,
+    CHANGE_LANGUAGE,
+    SET_ROLES,
+    CHANGE_THEME
+} from "./actions/globalActions";
+
+const initialState = {
+    roles: [],
+    categories: [],
+    theme: "dark",
+    language: "tr"
+}
+
+const globalReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_CATEGORY:
+            return {
+                ...state, categories: [...state.categories, action.payload]
+            }
+        case SET_ROLES:
+            return {
+                ...state, roles: [...state.roles, action.payload]
+            }
+        case CHANGE_LANGUAGE:
+            return {
+                ...state , language: state.language === "tr" ? "en" : "tr"
+            }
+
+        case CHANGE_THEME:
+            return {
+                ...state , theme: state.theme === "dark" ? "light" : "dark"
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default globalReducer;
