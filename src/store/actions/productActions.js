571918563174fdce026d90fe10cfe7931660252d
KrieqqeR <burakcevizli@gmail.com>
2023-10-23 22:16:52 +0300

    productActtions üzerinde calısılıyor.

--- src/store/actions/productActions.js
+++ src/store/actions/productActions.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export const SET_PRODUCT = "SET_PRODUCT"
 export const ADD_COUNT = "ADD_COUNT"
 export const REMOVE_COUNT = "REMOVE_COUNT"
@@ -5,6 +7,11 @@ export const ADD_PAGECOUNT = "ADD_PAGECOUNT"
 export const DELETE_PAGECOUNT = "DELETE_PAGECOUNT"
 export const SET_ACTIVEPAGE = "SET_ACTIVEPAGE"
 export const CHANGE_FETCH_STATE = "CHANGE_FETCH_STATE"
+export const FETCHED = "FETCHED";
+export const FETCHING = "“FETCHING”";
+export const NOT_FETCHED = "NOT_FETCHED";
+export const FAILED = "FAILED"
+
 
 export const setProducts = (product) => {
     return { type: SET_PRODUCT, payload: product }
@@ -24,6 +31,20 @@ export const deletePageCount = () => {
 export const setActivePage = (page) => {
     return { type: SET_ACTIVEPAGE, payload: page }
 }
-export const changeFetchState = (fetchState) => {
-    return { type: CHANGE_FETCH_STATE, payload: fetchState }
-}
+export const changeFetchState = () => (dispatch) => {
+    dispatch({ type: CHANGE_FETCH_STATE });
+  
+    axios
+      .get("https://workintech-fe-ecommerce.onrender.com/")
+      .then((res) => {
+        dispatch({ type: FETCH_SUCCESS, payload: res.data });
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        dispatch({ type: FAILED, payload: err.message });
+      })
+      .finally(() => {
+        dispatch({ type: FETCH_LOADING });
+      });
+  };
