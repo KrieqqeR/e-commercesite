e4164ca9d7d0d35328f7fb96a260204c64308233
KrieqqeR <burakcevizli@gmail.com>
2023-12-06 20:16:43 +0300

    Single page klaybolması dzeltiltid.

--- src/components/ProductListPageCards.js
+++ src/components/ProductListPageCards.js
@@ -9,96 +9,86 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function ProductListPageCards() {
     const productList = useSelector((select) => select.product.productList);
-    const loading = useSelector((select) => select.loading.boolean)
+    const loading = useSelector((select) => select.loading.boolean);
     const dispatch = useDispatch();
     const [offset, setOffset] = useState(0);
     const limit = 25;
-    const history = useHistory()
+    const history = useHistory();
 
-    const fetchMoreData = () => {
-        dispatch(setLoading(true))
-        setTimeout(() => {
-            api.get(`products/?offset=${offset}&limit=${limit}`)
-                .then((response) => {
-                    console.log("INFINITE RESPONSE , ", response);
-                    dispatch(setProductList(productList?.concat(response.data.products)));
-                    setOffset(offset + limit);
-                })
-                .catch((error) => {
-                    console.error("Error fetching more data:", error);
-                })
-                .finally(() => {
-                    dispatch(setLoading(false));
-                });
-        }, 1500)
+    const fetchMoreData = async () => {
+        dispatch(setLoading(true));
 
+        await api
+            .get(`products/?offset=${offset}&limit=${limit}`)
+            .then((response) => {
+                console.log('INFINITE RESPONSE , ', response);
+                dispatch(setProductList(productList?.concat(response.data.products)));
+                setOffset(offset + limit);
+            })
+            .catch((error) => {
+                console.error('Error fetching more data:', error);
+            })
+            .finally(() => {
+                dispatch(setLoading(false));
+            });
     };
 
     const onClickHandler = (data) => {
-        console.log("HİSTORYYY , DATA ", data)
-        history.push(`/${data.category_id}/${data.id}/${(data.name).split(" ").join("").toLowerCase()}`)
-
-
-    }
-
+        console.log('HİSTORYYY , DATA ', data);
+        history.push(`/${data.category_id}/${data.id}/${data.name.split(' ').join('').toLowerCase()}`);
+    };
 
     useEffect(() => {
         fetchMoreData();
     }, []);
 
     return (
+        <>
         <div className='w-[70rem] mobile:w-[20rem] flex flex-wrap mobile:flex-col mx-auto gap-8'>
-            <div className='w-[70rem] mobile:w-[20rem] flex flex-wrap mobile:flex-col mx-auto gap-8'>
-                <InfiniteScroll
-                    dataLength={productList.length}
-                    next={fetchMoreData}
-                    hasMore={true}
-                    loader={() => dispatch(setLoading(true))}
-                    className='w-[70rem] mobile:w-[20rem] flex flex-wrap mobile:flex-col mx-auto gap-8'
-                >
-                    {productList.map((eleman, index) => (
-                        <div onClick={() => onClickHandler(eleman)} key={index} className='cursor-pointer'>
-                            {eleman && (
-                                <>
-                                    {eleman.images && eleman.images[0] && (
-                                        <img src={eleman.images[0]?.url} alt='Product' className='mt-10 mobile:ml-16 w-[15rem] h-[15rem]' />
-                                    )}
-                                    <h2 className='text-[#252B42]  mt-4 text-center text-[1.1rem] font-bold'>{eleman.name}</h2>
-                                    <h4 className='mt-4 text-center w-[15rem] flex text-[#737373] text-[0.875] font-bold'>{eleman.description}</h4>
-                                    <p className='mt-4 text-center text-[#BDBDBD] text-[1rem] font-bold'>{eleman.price}₺<span className="text-[#23856D] ml-2">{eleman.rating}</span></p>
-                                </>
-                            )}
-                        </div>
-                    ))}
-                </InfiniteScroll>
-                <div className='flex justify-center mx-auto items-center mt-20'>
-                    <ClipLoader
-                        size={30}
-                        color={"#123abc"}
-                        loading={loading}
-                    />
-                </div>
-            </div>
-            {/* Page değiştirme yerleri. */}
-            <div className='flex mobile:ml-10 flex-wrap mx-auto mt-10'>
-                <div className='flex mobile:flex-col  flex-wrap w-[20rem] h-[4.5rem] justify-between border'>
-                    <button className='text-[#BDBDBD] bg-[#F3F3F3] p-[1.56rem] font-semibold'>
-                        First
-                    </button>
-                    <button className='text-[#23A6F0] bg-[#FFFFFF] hover:bg-[#23A6F0] hover:text-[#FFFFFF] px-[1.25rem] py-[1.56rem] font-semibold'>
-                        1
-                    </button>
-                    <button className='text-[#23A6F0] bg-[#FFFFFF] hover:bg-[#23A6F0] hover:text-[#FFFFFF]  px-[1.25rem] py-[1.56rem] font-semibold'>
-                        2
-                    </button>
-                    <button className='text-[#23A6F0] bg-[#FFFFFF] hover:bg-[#23A6F0] hover:text-[#FFFFFF]  px-[1.25rem] py-[1.56rem] font-semibold'>
-                        3
-                    </button>
-                    <button className='text-[#BDBDBD] bg-[#F3F3F3] p-[1.56rem] font-semibold'>
-                        Next
-                    </button>
-                </div>
+            <InfiniteScroll
+                dataLength={productList.length}
+                next={fetchMoreData}
+                hasMore={true}
+                loader={() => dispatch(setLoading(true))}
+                className='w-[70rem] mobile:w-[20rem] flex flex-wrap mobile:flex-col mx-auto gap-8'
+            >
+                {productList.map((eleman, index) => (
+                    <div onClick={() => onClickHandler(eleman)} key={index} className='cursor-pointer'>
+                        {eleman && (
+                            <>
+                                {eleman.images && eleman.images[0] && (
+                                    <img src={eleman.images[0]?.url} alt='Product' className='mt-10 mobile:ml-16 w-[15rem] h-[15rem]' />
+                                )}
+                                <h2 className='text-[#252B42] mt-4 text-center text-[1.1rem] font-bold'>{eleman.name}</h2>
+                                <h4 className='mt-4 text-center w-[15rem] flex text-[#737373] text-[0.875] font-bold'>{eleman.description}</h4>
+                                <p className='mt-4 text-center text-[#BDBDBD] text-[1rem] font-bold'>
+                                    {eleman.price}₺<span className='text-[#23856D] ml-2'>{eleman.rating}</span>
+                                </p>
+                            </>
+                        )}
+                    </div>
+                ))}
+            </InfiniteScroll>
+            <div className='flex justify-center mx-auto items-center mt-20'>
+                <ClipLoader size={30} color={'#123abc'} loading={loading} />
             </div>
         </div>
-    )
+        /* Page değiştirme yerleri. */
+        <div className='flex mobile:ml-10 flex-wrap mx-auto mt-10'>
+            <div className='flex mobile:flex-col  flex-wrap w-[20rem] h-[4.5rem] justify-between border'>
+                <button className='text-[#BDBDBD] bg-[#F3F3F3] p-[1.56rem] font-semibold'>First</button>
+                <button className='text-[#23A6F0] bg-[#FFFFFF] hover:bg-[#23A6F0] hover:text-[#FFFFFF] px-[1.25rem] py-[1.56rem] font-semibold'>
+                    1
+                </button>
+                <button className='text-[#23A6F0] bg-[#FFFFFF] hover:bg-[#23A6F0] hover:text-[#FFFFFF]  px-[1.25rem] py-[1.56rem] font-semibold'>
+                    2
+                </button>
+                <button className='text-[#23A6F0] bg-[#FFFFFF] hover:bg-[#23A6F0] hover:text-[#FFFFFF]  px-[1.25rem] py-[1.56rem] font-semibold'>
+                    3
+                </button>
+                <button className='text-[#BDBDBD] bg-[#F3F3F3] p-[1.56rem] font-semibold'>Next</button>
+            </div>
+        </div>
+        </>
+    );
 }
--- src/components/ProductPageSingleProduct.js
+++ src/components/ProductPageSingleProduct.js
@@ -1,39 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import Header from '../layouts/Header'
 import Footer from '../layouts/Footer'
 import { api } from '../api/api'
 import { setProductList } from '../store/actions/productActions'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import { useHistory } from 'react-router-dom'
 
 
 const ProductPageSingleProduct = () => {
     const dispatch = useDispatch();
     const history = useHistory()
-
+    const limit = 587;
     const { categoryid, productid, productNameSlug } = useParams()
-
     const singleProduct = useSelector((select) => select.product.productList)
-
     const eleman = singleProduct.find((tek) => tek.id == productid)
 
-    const onClickHandler = () =>{
+    const onClickHandler = () => {
         history.goBack()
     }
 
-    useEffect(()=>{
-        api.get("products/")
-        .then((response)=>{
-            dispatch(setProductList(response.data.products))
-        })
-    },[])
+
+
+    useEffect(() => {
+        async function fetchData() {
+          const response = await api.get(`products/?limit=${limit}`);
+          dispatch(setProductList(response.data.products));
+        }
+        fetchData();
+      }, []);
+
 
     return (
         <>
             <Header />
             <div className='cursor-pointer h-100vh'>
-                {eleman && (
+                {eleman ? (
                     <>
                         {eleman.images && eleman.images[0] && (
                             <img src={eleman.images[0]?.url} alt='Product' className='mt-10 mobile:ml-16 mx-auto w-[15rem] h-[15rem]' />
@@ -43,6 +45,8 @@ const ProductPageSingleProduct = () => {
                         <p className='mt-4 text-center text-[#BDBDBD] text-[1rem] font-bold'>{eleman.price}₺<span className="text-[#23856D] ml-2">{eleman.rating}</span></p>
                         <button className='justify-center items-center text-center mx-auto flex mt-4 p-4 bg-blue-500 text-white font-bold rounded' onClick={onClickHandler}>BACK PAGE</button>
                     </>
+                ) : (
+                    <p>Loading...</p>
                 )}
             </div>
             <Footer />
