6526ce73cad1d9718ecd6c4f2dcc5f3cb434f458
KrieqqeR <burakcevizli@gmail.com>
2023-12-16 16:47:19 +0300

    task 15 done ...

--- src/store/actions/shoppingCardActions.js
+++ src/store/actions/shoppingCardActions.js
@@ -1,14 +1,26 @@
-export const SET_LIST = "SET_LIST"
-export const DELETE_LIST = "DELETE_LIST"
-export const SET_PAYMENT = "SET_PAYMENT"
-export const SET_ADDRESS = "SET_ADDRESS"
+export const SET_LIST = "SET_LIST";
+export const DELETE_LIST = "DELETE_LIST";
+export const SET_PAYMENT = "SET_PAYMENT";
+export const SET_ADDRESS = "SET_ADDRESS";
+export const DECREASE_LIST = "DECREASE_LIST";
+export const FILTERED_PRODUCT = "FILTERED_PRODUCT";
+export const CHECKED_PRODUCT = "CHECKED_PRODUCT";
 
 export const addToList = (addedProduct) => {
-    return { type: SET_LIST, payload: addedProduct }
-}
+    return { type: SET_LIST, payload: addedProduct };
+};
 export const setPayment = (payment) => {
-    return { type: SET_PAYMENT, payload: payment }
-}
+    return { type: SET_PAYMENT, payload: payment };
+};
 export const setAddress = (address) => {
-    return { type: SET_ADDRESS, payload: address }
-}
+    return { type: SET_ADDRESS, payload: address };
+};
+export const decraseList = (decreasedProduct) => {
+    return { type: DECREASE_LIST, payload: decreasedProduct };
+};
+export const filteredProduct = (filteredProduct) => {
+    return { type: FILTERED_PRODUCT, payload: filteredProduct };
+};
+export const checkedProduct = (checkedProduct) => {
+    return { type: CHECKED_PRODUCT, payload: checkedProduct }
+};
--- src/store/reducers/shoppingCardReducer.js
+++ src/store/reducers/shoppingCardReducer.js
@@ -1,43 +1,98 @@
 import {
     SET_LIST,
     SET_PAYMENT,
-    SET_ADDRESS
-} from "../actions/shoppingCardActions"
-
-const initialState = {
+    SET_ADDRESS,
+    DECREASE_LIST,
+    FILTERED_PRODUCT,
+    CHECKED_PRODUCT,
+  } from "../actions/shoppingCardActions";
+  
+  const initialState = {
     card: [{ count: 0, checked: false, product: {} }],
     payment: {},
-    address: {}
-}
-
-const shoppingCardReducer = (state = initialState, action) => {
+    address: {},
+  };
+  
+  const shoppingCardReducer = (state = initialState, action) => {
     switch (action.type) {
-        case SET_LIST:
-            const existingProduct = state.card.findIndex(item => item.product.id === action.payload.id);
-            if (existingProduct !== -1) {
-                const updatedCard = [...state.card]
-                updatedCard[existingProduct] = {
-                    ...updatedCard[existingProduct], count: updatedCard[existingProduct].count + 1
-                }
-                return {
-                    ...state, card: updatedCard
-                }
-            } else {
-                return {
-                    ...state, card: [...state.card, { count: 1, checked: true, product: action.payload }]
-                }
-            }
-        case SET_PAYMENT:
+      case SET_LIST:
+        const existingProduct = state.card.findIndex(
+          (item) => item.product.id === action.payload.id
+        );
+        if (existingProduct !== -1) {
+          const updatedCard = [...state.card];
+          updatedCard[existingProduct] = {
+            ...updatedCard[existingProduct],
+            count: updatedCard[existingProduct].count + 1,
+          };
+          return {
+            ...state,
+            card: updatedCard,
+          };
+        } else {
+          return {
+            ...state,
+            card: [
+              ...state.card,
+              { count: 1, checked: true, product: action.payload },
+            ],
+          };
+        }
+      case CHECKED_PRODUCT:
+        const updatedCard = state.card.map((item) => {
+          if (item.product.id === action.payload.product.id) {
             return {
-                ...state, payment: action.payload
-            }
-        case SET_ADDRESS:
-            return {
-                ...state, address: action.payload
-            }
-        default:
-            return state;
+              ...item,
+              checked: !item.checked,
+            };
+          }
+          return item;
+        });
+  
+        return {
+          ...state,
+          card: updatedCard,
+        };
+  
+      case SET_PAYMENT:
+        return {
+          ...state,
+          payment: action.payload,
+        };
+      case SET_ADDRESS:
+        return {
+          ...state,
+          address: action.payload,
+        };
+      case DECREASE_LIST:
+        const decreasedProduct = state.card.findIndex(
+          (item) => item.product.id === action.payload.id
+        );
+        if (decreasedProduct !== -1) {
+          const updatedCard = [...state.card];
+          updatedCard[decreasedProduct] = {
+            ...updatedCard[decreasedProduct],
+            count:
+              updatedCard[decreasedProduct].count - 1 > 1
+                ? updatedCard[decreasedProduct].count - 1
+                : 1,
+          };
+          return {
+            ...state,
+            card: updatedCard,
+          };
+        }
+      case FILTERED_PRODUCT:
+        return {
+          ...state,
+          card: state.card.filter(
+            (cards) => cards.product.id !== action.payload.id
+          ),
+        };
+  
+      default:
+        return state;
     }
-}
-
-export default shoppingCardReducer;
+  };
+  
+  export default shoppingCardReducer;
