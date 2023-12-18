88df44ad412b7118b730170823f4526ce8541504
KrieqqeR <burakcevizli@gmail.com>
2023-12-18 14:11:23 +0300

    task 16 done .

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const OrderSummary = () => {
+    const [totalProductPrice, setTotalProductPrice] = useState(0)
+    const productList = useSelector((select) => select.shopping.card)
+
+    useEffect(() => {
+        const totalPrice = productList.reduce((acc, item) => {
+            if (item.checked) {
+                return acc + item.product.price * item.count;
+            }
+            return acc;
+        }, 0);
+
+        setTotalProductPrice(totalPrice);
+    }, [productList]);
     return (
         <div className="w-[30rem] h-[28rem] mt-[10rem] mx-auto border-8 border-violet-500">
             <button className="px-[6rem] py-4 ml-20 bg-violet-400 mt-7 text-white font-bold">Sepeti Onayla</button>
@@ -8,7 +22,7 @@ const OrderSummary = () => {
                 <h1 className="text-[2rem] font-bold text-black underline">Sipariş Özeti</h1>
                 <div className="flex text-black font-bold justify-between">
                     <h2>Ürünün Toplamı</h2>
-                    <p>Fiyat</p>
+                    <p>{totalProductPrice.toFixed(2)}₺</p>
                 </div>
                 <div className="flex text-black font-bold justify-between">
                     <h2>Kargo Toplamı</h2>
@@ -16,12 +30,12 @@ const OrderSummary = () => {
                 </div>
                 <div className="flex text-black font-bold justify-between">
                     <h2>150 TL Üstü ve Kargo Bedava</h2>
-                    <p>Fiyat</p>
+                    <p>{totalProductPrice >= 150 ? -35 : 35}₺</p>
                 </div>
                 <hr />
                 <div className="flex text-black font-bold justify-between">
                     <h2>Toplam</h2>
-                    <p>Fiyat</p>
+                    {totalProductPrice >=150 ? <p>{totalProductPrice.toFixed(2)}₺</p> : <p>{(totalProductPrice + 35).toFixed(2)}₺</p>}
                 </div>
             </div>
             <button className="px-[6rem] py-4 ml-20 mt-5 bg-violet-400 text-white font-bold">Sepeti Onayla</button>
