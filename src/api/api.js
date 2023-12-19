1300fdcd05f17da4821feda84c08b325bd81022a
KrieqqeR <burakcevizli@gmail.com>
2023-12-19 17:34:38 +0300

    api js güncellendi.

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const token = localStorage.getItem("token");
+
 export const api = axios.create({
   baseURL: "https://workintech-fe-ecommerce.onrender.com/",
+  headers: {
+    Authorization: token,
+  },
 });
