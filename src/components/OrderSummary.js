e5fce803a0acd360e080932534a67cf465709902
KrieqqeR <burakcevizli@gmail.com>
2023-12-21 19:42:37 +0300

    task 17 de hatalar var sor.

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 const OrderSummary = () => {
     const [totalProductPrice, setTotalProductPrice] = useState(0)
     const productList = useSelector((select) => select.shopping.card)
     const [totalKargo, setTotalKargo] = useState(0)
+    const token = localStorage.getItem("token")
+    const history = useHistory()
+
+    const onClickHandler = () => {
+        token ? history.push("completepaying") : history.push("login")
+    }
 
     useEffect(() => {
         const totalPrice = productList.reduce((acc, item) => {
@@ -20,7 +27,7 @@ const OrderSummary = () => {
     }, [productList]);
     return (
         <div className="w-[30rem] h-[28rem] mt-[10rem] mx-auto border-8 border-violet-500 fixed right-16">
-            <button className="px-[6rem] py-4 ml-20 bg-violet-400 mt-7 text-white font-bold">Sepeti Onayla</button>
+            <button className="px-[6rem] py-4 ml-20 bg-violet-400 mt-7 text-white font-bold" onClick={() => onClickHandler()}>Sepeti Onayla</button>
             <div className="border-1 w-[20rem] mt-4 mx-auto flex gap-4 flex-col">
                 <h1 className="text-[2rem] font-bold text-black underline">Sipariş Özeti</h1>
                 <div className="flex text-black font-bold justify-between">
@@ -47,7 +54,7 @@ const OrderSummary = () => {
                     {totalProductPrice >= 150 ? <p>{totalProductPrice.toFixed(2)}₺</p> : <p>{(totalProductPrice + totalKargo).toFixed(2)}₺</p>}
                 </div>
             </div>
-            <button className="px-[6rem] py-4 ml-20 mt-5 bg-violet-400 text-white font-bold">Sepeti Onayla</button>
+            <button className="px-[6rem] py-4 ml-20 mt-5 bg-violet-400 text-white font-bold" onClick={() => onClickHandler()}>Sepeti Onayla</button>
         </div>
     )
 }
--- src/components/PayingConfirmPage.js
+++ src/components/PayingConfirmPage.js
@@ -4,21 +4,26 @@ import { api } from '../api/api'
 import { useForm } from 'react-hook-form';
 
 export const PayingConfirmPage = () => {
-    const [addressArray, setAddressArray] = useState()
+    const [addressArray, setAddressArray] = useState("")
     const [isTrueOrFalse, setTrueOrFalse] = useState(false)
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+    const onSubmit = async (data) => {
+        console.log("Form data:", data);
+        try {
+            await api.post("/user/address", data);
+            console.log("Address submitted successfully!");
+        } catch (error) {
+            console.error("Error submitting address:", error);
+        }
+    };
 
     const onClickHandler = () => {
         setTrueOrFalse(!isTrueOrFalse)
     }
 
-
-
     useEffect(() => {
         api.get("user/address")
-            .then((response) => setAddressArray(response.data))
+            .then((response) => setAddressArray(response.data),console.log("addressArray , ", addressArray))
             .catch((error) => console.log("HATALI ADRESSS ,  , ", error))
     }, [])
 
@@ -47,15 +52,15 @@ export const PayingConfirmPage = () => {
                             <p className='text-[2rem] text-orange-500 font-bold text-center mt-8'>+</p>
                             <p className='font-bold text-black text-center '>Yeni Adres Ekle</p>
                         </div>
-                        {Array.from({ length: 3 }).map((_, i) => (
+                        {addressArray.map((each, i) => (
                             <div className='w-[30rem] h-[10rem] border-2 pl-4 pt-2 cursor-pointer' key={i}>
                                 <div className='flex justify-between mr-8 mt-4'>
-                                    <h1 className='font-bold text-black'>Ahmet Mehmet</h1>
-                                    <p className='text-black font-semibold'>TEL NO 183741824</p>
+                                    <h1 className='font-bold text-black'>{each.name}</h1>
+                                    <p className='text-black font-semibold'>{each.phone}</p>
                                 </div>
                                 <div>
-                                    <p className='mt-2 font-bold text-black'>YEŞİLOVA MAH . 40 61 SOKANO 4.1</p>
-                                    <p className='mt-2 font-bold text-black'>bornova izmiri</p>
+                                    <p className='mt-2 font-bold text-black'>{each.address}</p>
+                                    <p className='mt-2 font-bold text-black'>{each.neighborhood} / {each.city}</p>
                                 </div>
                             </div>
                         ))}
@@ -63,17 +68,20 @@ export const PayingConfirmPage = () => {
                             <div className='w-[30rem] mx-auto absolute left-[30rem] top-[10rem] bg-[#e2e8f0]'>
                                 <p className='text-purple-500 font-bold text-[1.5rem] mb-6'>Add New Address</p>
                                 <form className='flex gap-4 flex-wrap' onSubmit={handleSubmit(onSubmit)}>
-                                    <label className='flex gap-4 text-[1.2rem] font-bold text-orange-400'>Address:
-                                        <input className='border-2' type="text"  {...register} />
+                                    <label className='flex gap-4 text-[1.2rem] font-bold text-orange-400'>Address Title:
+                                        <input className='border-2' type="text"  {...register("title", {})} />
                                     </label>
-                                    <label className='flex gap-4 font-bold text-orange-400'>Name & Surname:
-                                        <input className='border-2' type="text"  {...register("Name & Surname", {})} />
+                                    <label className='flex gap-4 font-bold text-orange-400'>Name :
+                                        <input className='border-2' type="text"  {...register("name", {})} />
+                                    </label>
+                                    <label className='flex gap-4 font-bold text-orange-400'>Surname:
+                                        <input className='border-2' type="text"  {...register("surname", {})} />
                                     </label>
                                     <label className='flex gap-4 font-bold text-orange-400'>Phone:
-                                        <input className='border-2' type="text" {...register("Phone", {})} />
+                                        <input className='border-2' type="text" {...register("phone", {})} />
                                     </label>
                                     <label className='flex gap-4 font-bold text-orange-400'>City:
-                                        <select className='text-black' {...register("City")}>
+                                        <select className='text-black' {...register("city")}>
                                             <option value="Ankara">Ankara</option>
                                             <option value=" İzmir"> İzmir</option>
                                             <option value=" Kayseri"> Kayseri</option>
@@ -81,13 +89,13 @@ export const PayingConfirmPage = () => {
                                         </select>
                                     </label>
                                     <label className='flex gap-4 font-bold text-orange-400'>District:
-                                        <input className='border-2' type="text"  {...register("District", {})} />
+                                        <input className='border-2' type="text"  {...register("disctrict", {})} />
                                     </label>
                                     <label className='flex gap-4 font-bold text-orange-400'>Neighborhood:
-                                        <input className='border-2' type="text" {...register("Neighborhood", {})} />
+                                        <input className='border-2' type="text" {...register("neighborhood", {})} />
                                     </label>
                                     <label className='flex gap-4 font-bold text-orange-400'>Address 2:
-                                        <input className='border-2' type='text' {...register("Address", {})} />
+                                        <input className='border-2' type='text' {...register("address", {})} />
                                     </label>
                                     <button className='bg-green-600 text-white font-bold px-[13.8rem] mx-auto py-4 rounded-md ' type='submit'>Save</button>
                                 </form>
