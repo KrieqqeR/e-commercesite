68dd872a108f6d8bd255e74875c701838a4940c4
KrieqqeR <burakcevizli@gmail.com>
2024-01-04 22:38:38 +0300

    Çalışıyor.

--- src/components/PayingConfirmPage.js
+++ src/components/PayingConfirmPage.js
@@ -1,31 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useSyncExternalStore } from 'react'
 import OrderSummary from './OrderSummary'
 import { api } from '../api/api'
 import { useForm } from 'react-hook-form';
 
 export const PayingConfirmPage = () => {
-    const [addressArray, setAddressArray] = useState("")
+    const [addressArray, setAddressArray] = useState([])
     const [isTrueOrFalse, setTrueOrFalse] = useState(false)
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = async (data) => {
-        console.log("Form data:", data);
+    const [newAddress , setNewAddress] = useState(false)
+
+
+    const onSubmit = (data) => {
+        console.log("Form data " , data)
         try {
-            await api.post("/user/address", data);
-            console.log("Address submitted successfully!");
+            console.log("Burda try catch gircek mi")
+            api.post("/user/address", data).then((response) => {
+                console.log("Successfully submitted address:", response.data);
+                setNewAddress(false)
+                setTrueOrFalse(false)
+            });
+            console.log("BURDADA CIKMASI LAZIM")
         } catch (error) {
             console.error("Error submitting address:", error);
         }
     };
 
-    const onClickHandler = () => {
-        setTrueOrFalse(!isTrueOrFalse)
-    }
+
+    
+
+    
 
     useEffect(() => {
-        api.get("user/address")
-            .then((response) => setAddressArray(response.data),console.log("addressArray , ", addressArray))
-            .catch((error) => console.log("HATALI ADRESSS ,  , ", error))
-    }, [])
+        const fetchAddresses = async () => {
+            try {
+                const response = await api.get("user/address");
+                console.log("API response:", response);
+                setAddressArray(response.data);
+            } catch (error) {
+                console.log("Error fetching addresses:", error);
+            }
+        };
+
+        fetchAddresses();
+    }, [newAddress]);
 
     return (
         <div className='flex'>
@@ -48,19 +65,19 @@ export const PayingConfirmPage = () => {
                     </div>
                     <div className='flex flex-wrap gap-4 justify-between mt-10'>
 
-                        <div onClick={() => onClickHandler()} className='w-[30rem] h-[10rem] border-2 cursor-pointer'>
+                        <div onClick={() => setTrueOrFalse(true)} className='w-[30rem] h-[10rem] border-2 cursor-pointer'>
                             <p className='text-[2rem] text-orange-500 font-bold text-center mt-8'>+</p>
                             <p className='font-bold text-black text-center '>Yeni Adres Ekle</p>
                         </div>
                         {addressArray?.map((each, i) => (
                             <div className='w-[30rem] h-[10rem] border-2 pl-4 pt-2 cursor-pointer' key={i}>
                                 <div className='flex justify-between mr-8 mt-4'>
-                                    <h1 className='font-bold text-black'>{each.name}</h1>
-                                    <p className='text-black font-semibold'>{each.phone}</p>
+                                    <h1 className='font-bold text-black'>{each?.name}</h1>
+                                    <p className='text-black font-semibold'>{each?.phone}</p>
                                 </div>
                                 <div>
-                                    <p className='mt-2 font-bold text-black'>{each.address}</p>
-                                    <p className='mt-2 font-bold text-black'>{each.neighborhood} / {each.city}</p>
+                                    <p className='mt-2 font-bold text-black'>{each?.address}</p>
+                                    <p className='mt-2 font-bold text-black'>{each?.neighborhood} / {each?.city}</p>
                                 </div>
                             </div>
                         ))}
@@ -83,13 +100,14 @@ export const PayingConfirmPage = () => {
                                     <label className='flex gap-4 font-bold text-orange-400'>City:
                                         <select className='text-black' {...register("city")}>
                                             <option value="Ankara">Ankara</option>
-                                            <option value=" İzmir"> İzmir</option>
-                                            <option value=" Kayseri"> Kayseri</option>
-                                            <option value=" İstanbul"> İstanbul</option>
+                                            <option value="İzmir">İzmir</option>
+                                            <option value="Kayseri">Kayseri</option>
+                                            <option value="İstanbul">İstanbul</option>
+
                                         </select>
                                     </label>
                                     <label className='flex gap-4 font-bold text-orange-400'>District:
-                                        <input className='border-2' type="text"  {...register("disctrict", {})} />
+                                        <input className='border-2' type="text"  {...register("district", {})} />
                                     </label>
                                     <label className='flex gap-4 font-bold text-orange-400'>Neighborhood:
                                         <input className='border-2' type="text" {...register("neighborhood", {})} />
@@ -97,7 +115,7 @@ export const PayingConfirmPage = () => {
                                     <label className='flex gap-4 font-bold text-orange-400'>Address 2:
                                         <input className='border-2' type='text' {...register("address", {})} />
                                     </label>
-                                    <button className='bg-green-600 text-white font-bold px-[13.8rem] mx-auto py-4 rounded-md ' type='submit'>Save</button>
+                                    <button onClick={() => setNewAddress(true)} className='bg-green-600 text-white font-bold px-[13.8rem] mx-auto py-4 rounded-md ' type='submit'>Save</button>
                                 </form>
                             </div>
                         }
